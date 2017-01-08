40beb6358d9f43f8514f3996f89abcc6fa9971dc
Parth Patel <ParthPatel1998@users.noreply.github.com>
2017-01-08 13:40:42 -0500

    Delete Variables.js

--- Variables.js
+++
@@ -1,8 +0,0 @@
-var snake;  // snake variable to represent the snake object
-var food;  // food variable to represent the food object
-var scl = 30;
-var SnakeSize = 30;
-var fps = 10;
-var FoodSize = 30;
-var score = document.getElementById("Score");
-var TotalScore = 1;
