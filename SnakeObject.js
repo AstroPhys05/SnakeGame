1e00120bc6574758ea49385a2f3c656ff9281793
Parth Patel <ParthPatel1998@users.noreply.github.com>
2017-01-08 10:21:29 -0500

    Add files via upload

---
+++ SnakeObject.js
@@ -0,0 +1,28 @@
+// Snake Constructor function
+function Snake() {
+  // initial values of the snake
+  this.x = 300;
+  this.y = 300;
+  this.xspeed = 0;
+  this.yspeed = 0;
+
+// Draws the Snake using p5 on the canvas
+  this.show = function() {
+    fill(255);
+    rect(this.x, this.y, SnakeSize, SnakeSize);
+    this.x = constrain(this.x, 0, width - SnakeSize);
+    this.y = constrain(this.y, 0, height - SnakeSize);
+  }
+
+// Moves the Snake around the grid based on itsx, y speeds
+  this.update = function() {
+    this.x = this.x + this.xspeed * SnakeSize;
+    this.y = this.y + this.yspeed * SnakeSize;
+  }
+
+// changes the speed in the direction given to the function
+  this.direction = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+}
---
+++ Variables.js
@@ -0,0 +1,6 @@
+var snake;  // snake variable to represent the snake object
+var food;  // food variable to represent the food object
+var scl = 30;
+var SnakeSize = 30;
+var fps = 10;
+var FoodSize = 30;
