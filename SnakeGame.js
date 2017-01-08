99a0a15ff9475f7efaaac3c656021da903d19110
Parth Patel <ParthPatel1998@users.noreply.github.com>
2017-01-08 12:14:18 -0500

    Add files via upload

--- SnakeGame.js
+++ SnakeGame.js
@@ -1,5 +1,6 @@
 // setup function by p5 framework to give us the
 //    setup of the canvas and the snake object
+
 function setup(){
   createCanvas(600,600);
   snake = new Snake();
@@ -7,16 +8,20 @@ function setup(){
   frameRate(fps);
 }
 
+
 // p5 draw function draws the background and the snake
 function draw() {
   background(50);
+  if (snake.eat(food)) {
+    food = new Food();
+    snake.total++;
+  }
   snake.update();
   snake.show();
   food.show();
 
-  if (snake.eat(food)) {
-    food = new Food();
-  }
+  fill(150);
+  rect(food.x, food.y, scl, scl);
 }
 
 // Keyboard Controls of the snake
